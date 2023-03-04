0482db4af2e7dda51b86a68067ef834969f2eb37
Semanticbits <faraazmohdkhan@gmail.com>
2023-03-04 14:09:48 +0530

    email test

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
+const path = require('path');
 var cors = require("cors");
+var hbs = require('nodemailer-express-handlebars');
 
 dotenv.config();
 const app = express();
@@ -12,12 +14,34 @@ const port = process.env.PORT || 8281;
 app.use(cors());
 app.use(bodyParser.json());
 
-// Create a webhook endpoint to receive requests from mudukaan.io
+
+const handlebarOptions = {
+  viewEngine: {
+    extName: ".handlebars",
+    partialsDir: path.resolve('./views'),
+    defaultLayout: false,
+  },
+  viewPath: path.resolve('./views'),
+  extName: ".handlebars",
+}
+
+
+
 app.post('/webhook', (req, res) => {
    console.log(req.body);
   // Extract the order details from the request body
-  const { customerName, email, phone, items } = req.body;
-console.log(customerName, email, phone, items);
+  const { order } = req.body;
+console.log(order);
+let customer_fullname = order.customer.full_name;
+let order_number = order.id;
+let order_status_link = order.order_status_url.order_status_url;
+let shipmentAddress = order.shipping_address.address1 + ' ' + order.shipping_address.address2 + ', ' + order.shipping_address.city + ', Pincode -' + order.shipping_address.zip;
+let mobile = order.shipping_address.phone;
+let item_price = order.total_line_items_price;
+let total_price = order.current_total_price;
+let payment_method = order.gateway;
+
+
   // Send an email notification to the customer
   const transporter = nodemailer.createTransport({
     host: 'smtp.zoho.in',
@@ -28,12 +52,20 @@ console.log(customerName, email, phone, items);
       pass: `${process.env.EMAIL_PASS}`
     }
   });
-
+  transporter.use('compile', hbs(handlebarOptions));
   const mailOptions = {
     from: `${process.env.EMAIL_USER}`,
     to: email,
     subject: 'Your Order Details',
-    text: `Hi ${customerName},\n\nThank you for your order. Here are the details:\n\n${items}\n\nIf you have any questions, please reply to this email.\n\nBest regards,\n\nYour Name`
+    template: 'email',
+  context: {
+    name: customer_fullname,
+    track_order_link:order_status_link,
+    order_number: order_number,
+    order_subtotal: item_price,
+    order_total : total_price,
+    payment_mode : payment_method
+  }
   };
 
   transporter.sendMail(mailOptions, (err, info) => {
